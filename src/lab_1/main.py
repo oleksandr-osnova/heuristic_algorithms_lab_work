3d8a7282a735df67c9a2f16c4333dafedd9ba619
oleksandr-osnova <alex.osnova.work@gmail.com>
2024-11-17 14:57:19 +0200

    Update solution for lab_1 - make max_no_improve dynamic

--- src/lab_1/main.py
+++ src/lab_1/main.py
@@ -6,19 +6,31 @@ import utils
 # Головна функція для запуску обчислювального експерименту
 def main():
     number_of_starts = 20
-    tsp_problem = tsplib95.load(str(utils.input_data_folder / 'a280.tsp'))
-    tsp_problem_solution = tsplib95.load(str(utils.input_data_folder / 'a280.opt.tour'))
+    interactive_plot = False
+    initial_plot_pause = 6
+    tsp_problem_file_name = 'a280.tsp'
+    tsp_problem_solution_file_name = 'a280.opt.tour'
+
+    tsp_problem = tsplib95.load(str(utils.input_data_folder / tsp_problem_file_name))
 
     cities = tsp_problem.node_coords
     city_coords = np.array([*cities.values()])
 
-    tsp_problem_solution_tour = np.array([cities[city] for city in tsp_problem_solution.tours[0]])
-    tsp_problem_solution_tour_distance = utils.calculate_distance(tsp_problem_solution_tour)
+    if tsp_problem_solution_file_name:
+        tsp_problem_solution = tsplib95.load(str(utils.input_data_folder / tsp_problem_solution_file_name))
 
-    plt.figure('Рішення')
-    utils.plot_route(tsp_problem_solution_tour, f"Рішення, Дистанція: {tsp_problem_solution_tour_distance:.2f}", block = False, pause = 6)
+        tsp_problem_solution_tour = np.array([cities[city] for city in tsp_problem_solution.tours[0]])
+        tsp_problem_solution_tour_distance = utils.calculate_distance(tsp_problem_solution_tour)
 
-    best_route, best_distance, worst_route, worst_distance, results = utils.hill_climbing_multi_start(city_coords, number_of_starts, interactive_plot=False)
+        plt.figure('Рішення')
+        utils.plot_route(
+            tsp_problem_solution_tour, f"Рішення, Дистанція: {tsp_problem_solution_tour_distance:.2f}",
+            block = False, pause = initial_plot_pause)
+
+    iteration_figure = plt.figure('Ітерація')
+    best_route, best_distance, worst_route, worst_distance, results = utils.hill_climbing_multi_start(
+        city_coords, number_of_starts, interactive_plot=interactive_plot, first_pause = initial_plot_pause)
+    plt.close(iteration_figure)
 
     plt.figure('Hill Climbing')
     utils.plot_route(best_route, f"Ітерація: {number_of_starts or 'остання'}, Дистанція: {best_distance:.2f}")
--- src/lab_1/utils.py
+++ src/lab_1/utils.py
@@ -7,8 +7,10 @@ data_folder = pathlib.Path('../../data/lab_1')
 input_data_folder = data_folder / 'input'
 output_data_folder = data_folder / 'output'
 
+
 def generate_random_route(cities):
     return np.array(random.sample(list(cities), len(cities)))
+
 
 # Визначаємо функцію для обчислення довжини шляху
 def calculate_distance(route, cycled=True):
@@ -21,9 +23,11 @@ def calculate_distance(route, cycled=True):
     # Обчислюємо довжину маршруту за допомогою евклідової відстані
     return np.sum(np.sqrt(np.sum(np.diff(route_extended, axis=0) ** 2, axis=1)))
 
+
 # Застосовує 2-opt до маршруту, створюючи новий маршрут із реверсом підмаршруту.
 def two_opt(route, i, j):
     return np.vstack([route[:i], route[i:j][::-1], route[j:]])
+
 
 #Обчислює зміну довжини маршруту після застосування 2-opt
 def incremental_distance_update(route, i, j):
@@ -35,9 +39,10 @@ def incremental_distance_update(route, i, j):
 
     return added - removed
 
-def hill_climbing(cities, max_no_improve = None):
+
+def hill_climbing(cities, max_no_improve=None):
     num_cities = len(cities)
-    max_no_improve = max(max_no_improve or num_cities * 5, 100)
+    max_no_improve = max_no_improve or calculate_max_no_improve_tries(num_cities, 1.2, 20, 5)
 
     current_route = generate_random_route(cities)
     current_distance = calculate_distance(current_route)
@@ -71,7 +76,9 @@ def hill_climbing(cities, max_no_improve = None):
 
     return best_route, best_distance
 
-def hill_climbing_multi_start(cities, num_starts = None, max_no_improve = None, interactive_plot = False):
+
+def hill_climbing_multi_start(
+        cities, num_starts=None, max_no_improve=None, interactive_plot=False, block=True, first_pause = 6, pause = 0.5):
     num_starts = num_starts or round(len(cities) * 0.2)
 
     best_route = None
@@ -80,6 +87,10 @@ def hill_climbing_multi_start(cities, num_starts = None, max_no_improve = None, 
     worst_distance = float(0)
     results = []
     iteration = 0
+
+    max_no_improve = max_no_improve or calculate_max_no_improve_tries(len(cities), 1.2, 20, 5)
+
+    print(f"Максимальна кількість ітерацій без покращень = {max_no_improve}")
 
     if interactive_plot:
         plt.ion()  # Вмикаємо інтерактивний режим
@@ -90,15 +101,23 @@ def hill_climbing_multi_start(cities, num_starts = None, max_no_improve = None, 
 
         if current_distance < best_distance:
             best_route, best_distance = current_route, current_distance
-        elif current_distance > worst_distance:
+        if current_distance > worst_distance:
             worst_route, worst_distance = current_route, current_distance
 
         iteration += 1
 
         if iteration % 1 == 0:
             if interactive_plot:
-                plot_route(best_route, best_distance, iteration, interactive_plot)
-            print(f"Ітерація = {iteration}: Найкращий маршрут (довжина = {best_distance:.2f}), Найгірший маршрут (довжина = {worst_distance:.2f})")
+                plot_pause = first_pause if iteration == 1 else pause
+                plot_route(
+                    best_route, f"Ітерація: {iteration}, Дистанція: {best_distance:.2f}",
+                    interactive_plot=interactive_plot, block = block, pause = plot_pause)
+            print(
+                f"Ітерація = {str(iteration).zfill(len(str(num_starts)))}: "
+                f"Наявний маршрут (довжина = {current_distance:.2f}), "
+                f"Найкращий маршрут (довжина = {best_distance:.2f}), "
+                f"Найгірший маршрут (довжина = {worst_distance:.2f})"
+            )
 
     if interactive_plot:
         plt.ioff()  # Вимикаємо інтерактивний режим
@@ -106,7 +125,8 @@ def hill_climbing_multi_start(cities, num_starts = None, max_no_improve = None, 
 
     return best_route, best_distance, worst_route, worst_distance, results
 
-def plot_route(route, title, interactive_plot = False, block = True, pause = 0.5):
+
+def plot_route(route, title, interactive_plot=False, block=True, pause=0.5):
     plt.clf()
 
     # Замикаємо маршрут, зоб повернутися в початкову точку
@@ -138,6 +158,7 @@ def plot_route(route, title, interactive_plot = False, block = True, pause = 0.5
         plt.show(block=block)
         plt.pause(pause)
 
+
 def plot_histogram(results):
     plt.figure()
     plt.hist(results, bins=20, color='blue', alpha=0.7, edgecolor='black')
@@ -146,19 +167,27 @@ def plot_histogram(results):
     plt.ylabel("Частота")
     plt.show()
 
+
 def calculate_distance_difference(best_distance, worst_distance):
     return worst_distance - best_distance
+
 
 def calculate_route_distance_difference(best_route, worst_route):
     best_distance = calculate_distance(best_route)
     worst_distance = calculate_distance(worst_route)
     return calculate_distance_difference(best_distance, worst_distance)
 
+
 def plot_convergence(results):
     plt.figure()
     plt.plot(range(len(results)), sorted(results, reverse=True), marker='o', linestyle='-', color='green')
-    plt.title("График сходимости целевой функции")
+    plt.title("Графік збіжності цільової функції")
     plt.xlabel("Запуски")
-    plt.ylabel("Лучшая длина маршрута")
+    plt.ylabel("Найкраща довжина маршруту")
     plt.grid()
     plt.show()
+
+
+def calculate_max_no_improve_tries(num, scale=1.25, min_count=20, multiplayer=5):
+    fraction = max((int(num - (num * 0.1) ** scale)), 0)
+    return (fraction + min_count) * multiplayer
